43deedddb5856e08960ec7a8a5d825d553eb0be3
Eathan <eathanlangford@gmail.com>
2019-03-20 08:49:24 +1100

    initial commit

---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class nav extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <div className="logo">
+        
+        </div>
+        
+      </div>
+    )
+  }
+}
